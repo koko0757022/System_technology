bc749238ee6a997767add3c7dee08ed46ab70e77
Dong hwan lim <koko0757022@naver.com>
2017-03-31 02:22:28 +0000

    Mission2

--- Mission2/Mission2.py
+++ Mission2/Mission2.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import time
+from urllib.request import urlopen
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
@@ -21,31 +22,8 @@ def read_temp():
   temp_string = lines[1][equals_pos+2:]
   temp_c = float(temp_string) / 1000.0
   temp_f = temp_c * 9.0 / 5.0 + 32.0
-  return temp_c, temp_f
+  return temp_c
 while True:
  print(read_temp())
- time.sleep(1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ content = urlopen("https://api.thingspeak.com/update?api_key=X9EZDZONZ1W6WTK7&field1="+str(read_temp()))
+ time.sleep(13)
